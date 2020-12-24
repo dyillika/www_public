cb970e8a09ae9089da0a6fe09eeab8c582df1670
dyillika <dyillika@mail.ru>
2020-12-24 15:48:07 +0300

    init

--- money6/cont.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('button');
-const form = document.querySelector('#blablabla');
-const popup = document.querySelector('.popup');
-
-button.addEventListener('click', () => {
-    form.classList.add('open');
-    popup.classList.add('popup_open');
-});
